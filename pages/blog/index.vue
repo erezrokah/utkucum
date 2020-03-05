3f5aab24ac4a78cf252433e99fc042796944a1c1
Okan Sahin <okansahinfed@gmail.com>
2020-03-05 15:18:41 +0300

    fix bloglink

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
         <li v-for="(blogPost, index) in blogPosts" :key="`post-${index}`">
-            <nuxt-link to="{{blogPost.slug}}"><h1>{{blogPost.title}}</h1></nuxt-link>
+            <nuxt-link :to="blogPost.slug"><h1>{{blogPost.title}}</h1></nuxt-link>
             <div>{{blogPost.body}}</div>
         </li>
     </ul>
