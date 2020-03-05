08ae804011881ec576f03b2478623ec6ba6e9ef8
Okan Sahin <okansahinfed@gmail.com>
2020-03-05 15:27:13 +0300

    blogpost body on blog/index.vue fixed

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <ul>
         <li v-for="(blogPost, index) in blogPosts" :key="`post-${index}`">
             <nuxt-link :to="blogPost.slug"><h1>{{blogPost.title}}</h1></nuxt-link>
-            <div>{{blogPost.body}}</div>
+            <div v-html="$md.render(blogPost.body)" />
         </li>
     </ul>
 </template>
