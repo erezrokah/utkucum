3dc208f0408853bb7f3c0f4082f7c4ea64cd1e44
Okan Sahin <okansahinfed@gmail.com>
2020-03-06 02:44:48 +0300

    back to regular

--- store/index.js
+++ store/index.js
@@ -1,14 +1,10 @@
 export const state = () => ({
     blogPosts: [],
-    workPosts: []
   });
   
   export const mutations = {
     setBlogPosts(state, list) {
       state.blogPosts = list;
-    },
-    setWorkPosts(state, list) {
-      state.workPosts = list;
     },
   };
   
@@ -21,14 +17,5 @@ export const state = () => ({
         return res;
       });
       await commit('setBlogPosts', blogPosts);
-    },
-    async nuxtServerInit({ commit }) {
-      let files = await require.context('~/assets/content/work/', false, /\.json$/);
-      let workPosts = files.keys().map(key => {
-        let res = files(key);
-        res.slug = key.slice(2, -5);
-        return res;
-      });
-      await commit('setWorkPosts', workPosts);
     },
   };
