b740940ab24fb085396d8178daaa01f75aae0127
Okan Sahin <okansahinfed@gmail.com>
2020-03-06 01:58:06 +0300

    added new work collection

---
+++ pages/work/_work.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>{{workPost.title}}</h1>
+    <div v-html="$md.render(workPost.body)" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { workPost: payload };
+    else
+      return {
+        workPost: await require(`~/assets/content/work/${params.work}.json`),
+      };
+  },
+};
+</script>
+
+<style>
+
+</style>
--- store/index.js
+++ store/index.js
@@ -1,10 +1,14 @@
 export const state = () => ({
     blogPosts: [],
+    workPosts: []
   });
   
   export const mutations = {
     setBlogPosts(state, list) {
       state.blogPosts = list;
+    },
+    setWorkPosts(state, list) {
+      state.workPosts = list;
     },
   };
   
@@ -17,5 +21,14 @@ export const state = () => ({
         return res;
       });
       await commit('setBlogPosts', blogPosts);
+    },
+    async nuxtServerInit({ commit }) {
+      let files = await require.context('~/assets/content/work/', false, /\.json$/);
+      let blogPosts = files.keys().map(key => {
+        let res = files(key);
+        res.slug = key.slice(2, -5);
+        return res;
+      });
+      await commit('setWorkPosts', workPosts);
     },
   };
