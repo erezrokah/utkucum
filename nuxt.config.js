1aaf0e03306b227172323ba0a9104927c639afa3
Okan Sahin <okansahinfed@gmail.com>
2020-03-06 14:26:27 +0300

    minor fix on config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ var path = require('path');
 // The Nuxt routes are generate by Nuxt automatically based on the pages folder.
 var dynamicRoutes = getDynamicPaths({
   '/blog': 'blog/*.json',
-  '/page': 'work/*.json',
+  '/work': 'work/*.json',
 });
 
 export default {
