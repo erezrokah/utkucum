120d852d0b8e10ca0568612d429c2912bc73930d
Okan Sahin <okansahinfed@gmail.com>
2020-03-06 14:23:29 +0300

    fix on store

--- store/index.js
+++ store/index.js
@@ -1,43 +1,48 @@
 export const state = () => ({
-    blogPosts: [],
-    workPosts:[]
-  });
-  
-  export const mutations = {
-    SET_POSTS(state, data) {
-      state.blogPosts = data
-    },
-    SET_WORKPOSTS(state, data) {
-      state.workPosts = data
-    },
-  };
-  
-  export const actions = {
-    async nuxtServerInit({ dispatch }) {
-      await dispatch('getBlogPosts')
-      await dispatch('getWorkPosts')
-    },
-    async getBlogPosts({ state, commit }) {
-      const context = await require.context('~/content/blog/', false, /\.json$/);
+  blogPosts: [],
+  workPosts:[]
+});
 
-      const searchposts = await context.keys().map(key => ({
-        ...context(key),
-        _path: `/blog/${key.replace('.json', '').replace('./', '')}`
-      }));
+export const mutations = {
+  SET_POSTS(state, data) {
+    state.blogPosts = data
+  },
+  SET_WORKPOSTS(state, data) {
+    state.workPosts = data
+  },
+};
 
-      commit('SET_POSTS', searchposts.reverse())
-    },
-    async getWorkPosts({ state, commit }) {
+export const actions = {
+  async nuxtServerInit({ dispatch }) {
+    await dispatch('getBlogPosts')
+    await dispatch('getWorkPosts')
+  },
+  async getBlogPosts({ state, commit }) {
+    let files = await require.context(
+      '~/assets/content/blog/',
+      false,
+      /\.json$/
+    )
+    let blogPosts = files.keys().map(key => {
+      let res = files(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
+    await commit('SET_POSTS', blogPosts)
+  },
+  async getWorkPosts({ state, commit }) {
+    let wfiles = await require.context(
+      '~/assets/content/work/',
+      false,
+      /\.json$/
+    )
+    let workPosts = wfiles.keys().map(key => {
+      let res = wfiles(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
 
+    commit('SET_WORKPOSTS', workPosts)
 
-      const context = await require.context('~/content/work/', false, /\.json$/);
-
-      const workPosts = await context.keys().map(key => ({
-        ...context(key),
-        _path: `/work/${key.replace('.json', '').replace('./', '')}`
-      }));
-
-      commit('SET_WORKPOSTS', workPosts)
-
-    },
-  };
+  },
+};
